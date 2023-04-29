b247d1d597c20f9e2e642030e937557adf362285
ADMIN <alina.velichko1@mail.ru>
2023-04-29 14:24:54 +0300

    str_func changed

--- str_func.py
+++ str_func.py
@@ -1,4 +1,9 @@
 def reverse_str(string: str):
+    """Функция, переводящая каждую букву текста в верхний регистр"""
     return string.upper()
+
+def title_str(string: str):
+    """Функция, переводящая первую букву каждого слова в верхний регистр"""
+    return string.title()
 
 
